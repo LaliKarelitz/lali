44a283bf5bb7fc41ae9a57fb6eed4ce334a63734
LaliKarelitz <lali295203@gmail.com>
2022-11-16 03:39:53 +0200

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
+import Shoe from 'src/models/Sheo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lesson_1';
+  // title = 'lesson_1';
+  arrInStock: Shoe[] = [
+    new Shoe(1, "papaya", "123", false, 100,"../assets/carlogo.png"),
+    new Shoe(2, "papaya", "lll", false, 80,"../assets/carlogo.png"),
+    new Shoe(3, "nine west", "879", false, 230,"../assets/carlogo.png"),
+  ]
+  arrInSale: Shoe[] = [
+    new Shoe(1, "papaya", "123", false, 100,"../assets/carlogo.png"),
+    new Shoe(2, "papaya", "123", false, 80,"../assets/carlogo.png"),
+  ]
+  arrBasket: Shoe[] = [
+  ];
+
+  addToBasket(id: any) {
+    console.log(id, 'im herw=e')
+    let index = this.arrInStock.findIndex(o => o.id == id)
+    this.arrBasket.push(this.arrInStock[index])
+  }
+  removeFromBasket(id: any){
+    console.log('llllll')
+    let index=this.arrBasket.findIndex(i=>i.id==id);
+    this.arrBasket.splice(index,1);
+  }
 }
