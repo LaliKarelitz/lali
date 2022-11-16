9e4d751daf12d25a616d0d34219892792603140a
LaliKarelitz <lali295203@gmail.com>
2022-11-16 03:27:21 +0200

    fix

---
+++ src/models/Sheo.ts
@@ -0,0 +1,5 @@
+export default class Shoe {
+    constructor(public id: number, public company: string, public model: string, public isHeel: boolean, public price: number, public imgUrl=""){
+        
+    }
+}
